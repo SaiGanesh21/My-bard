81f906255824022f90264ea288b3210507ff0220
Sai Ganesh <kannams@iitbhilai.ac.in>
2023-07-01 02:52:32 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   try {
-    const response = await fetch("http://localhost:5000", {
+    const response = await fetch("https://my-bard.onrender.com/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
